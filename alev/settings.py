c73ee11e82f42faef3151d68c3ddbac43135d637
Serhii Afonin <serhij.afonin@gmail.com>
2023-08-20 16:35:10 -0300

    deploying changes for heroku

--- alev/settings.py
+++ alev/settings.py
@@ -1,4 +1,8 @@
 from pathlib import Path
+import django_heroku
+
+# Configure Django App for Heroku.
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -36,6 +40,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'alev.urls'
